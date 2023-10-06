876637d8860e39b6d5e2f15fb81416dc65f795dd
repl1307 <144289085+repl1307@users.noreply.github.com>
2023-10-05 23:16:24 -0400

    Update smallScreen.js
    update to es6 modules

--- scripts/game/smallScreen.js
+++ scripts/game/smallScreen.js
@@ -1,54 +1,49 @@
-function handleSmallScreen(){
+export default function handleSmallScreen(){
   const gameContainer = document.querySelector('.game-container');
   const mainContainer = document.querySelector('.main-container');
   const clickerContainer = document.querySelector('.clicker-container');
   const upgradeContainer = document.querySelector('.upgrades-container');
-  if(innerWidth <= 700){
+  
+  if(innerWidth <= 750){
     mainContainer.style.height = 'auto';
     gameContainer.style.flexDirection = 'column';
     upgradeContainer.style.minWidth = '100%';
     upgradeContainer.style.display = 'flex';
     upgradeContainer.style.overflowX = 'hidden';
     gameContainer.style.height = 'fit-content';
-    //gameContainer.style.overflowY = 'scroll';
-    //mainContainer.style.overflowY = 'scroll';
     document.body.style.overflowY = 'visible';
     document.body.style.overflowX = 'hidden';
     clickerContainer.style.width = '100vw';
-    clickerContainer.style.height = '90vh';
+    clickerContainer.style.minHeight = '95vh';
     document.querySelector('.game-header').style.height = '8vh';
     document.querySelector('.clicker-image-container button').style.height = '50vmin';
     document.querySelector('.clicker-image-container button').style.width = '100%';
     document.getElementById('reset-storage').style.bottom = '10px';
-    //document.querySelector('.clicker-image-container button').style.width = '90%';
-    //document.querySelector('.clicker-image-container img').style.height = '100%';
+    document.querySelector('.game-header').style.fontSize = '8vmin';
+    document.querySelector('.clicker-container h1').style.fontSize = '6vmin';
+    document.querySelector('.clicker-container p').style.fontSize = '5vmin';
+    document.querySelector('.clicker-image-container').style.marginTop = '50%';
   }
   else{
+    document.querySelector('.clicker-image-container button').style.height = '50min';
+    document.querySelector('.clicker-image-container button').style.width = '100%';
     mainContainer.style.height = '100vh';
     gameContainer.style.flexDirection = 'row';
     upgradeContainer.style.minWidth = '30%';
     upgradeContainer.style.display = 'flex';
-    gameContainer.style.height = '90vh';
     gameContainer.style.overflow = 'hidden';
     mainContainer.style.overflow = 'hidden';
     document.body.style.overflow = 'hidden';
     clickerContainer.style.width = '70%';
-    clickerContainer.style.height = '90vh';
-    upgradeContainer.style.height = '90vh';
+    clickerContainer.style.minHeight = '0';
+    clickerContainer.style.width = 'auto';
     document.querySelector('.game-header').style.height = '';
-    // document.querySelector('.clicker-image-container button').style.height = '50vmin';
-    document.querySelector('.clicker-image-container button').style.width = '75%';
+    document.querySelector('.clicker-image-container').style.marginTop = 0;
     document.getElementById('reset-storage').style.bottom = '15px';
-   // document.querySelector('.clicker-image-container button img').style.height = '100%';
+    document.querySelector('.game-header').style.fontSize = '6vmin';
+    document.querySelector('.clicker-container h1').style.fontSize = '4vmin';
+    document.querySelector('.clicker-container p').style.fontSize = '3.5vmin';
   }
-  // const h1 = document.querySelector('.clicker-container h1');
-  // const style = getComputedStyle(h1);
-  // while(h1.scrollWidth > h1.clientWidth){
-  //   h1.style.fontSize = (parseFloat(style.fontSize.replace('px',''))-1)+'px';
-  //   console.log(getComputedStyle(h1.style.fontSize))
-  // }
-  // console.log(h1.scrollWidth, h1.clientWidth)
+
 }
 
-window.addEventListener('resize', handleSmallScreen);
-handleSmallScreen();
